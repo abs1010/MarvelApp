3be06153cf8439562c2f19344fd598c43b8a4f27
Alan Silva <abs10@globomail.com>
2020-02-19 23:43:03 -0300

    Initial settings and gitignore

---
+++ MarvelApp/APIManager/File.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  MarvelApp
+//
+//  Created by Alan Silva on 18/02/20.
+//  Copyright © 2020 Alan Silva. All rights reserved.
+//
+
+import Foundation
+
+
+let publicKey = "0ecb2efd9039475d8d69e0caa59022b9"
+let privateKey = "0913068b512e6a4dfb4460d69f95c507421c2ac8"
+
+class DataProvider {
+    
+    
+    
+}
+
---
+++ MarvelApp/View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Alan Silva on 17/02/20.
+//  Copyright © 2020 Alan Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
